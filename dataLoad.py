99a7e2f4a4996f7f0a43ce84351029ac691cddd6
zburrows <zachariahnburrows@gmail.com>
2024-12-14 19:54:19 -0500

    working on data format and nn architecture

--- dataLoad.py
+++ dataLoad.py
@@ -4,6 +4,9 @@ import numpy as np
 from torch import nn
 import torch
 from makemidi import makeMidi
+
+# some code from https://medium.com/@abhilashkrish/step-by-step-guide-to-music-generation-using-rnns-with-pytorch-2fbf1a4172a3
+
 def find_instruments(ins, db = 'assets/wjazzd.db'): # Finds melids of solos of a certain instrument
     # access data from sq database
     con = sqlite3.connect(db)
@@ -12,16 +15,17 @@ def find_instruments(ins, db = 'assets/wjazzd.db'): # Finds melids of solos of a
 
     # Find all melid's with instrument
     melid = []
+    avgtempo = []
     for i in range(456):
         if solo_info['instrument'][i] == str(ins):
             melid += [solo_info['melid'][i]]
+            avgtempo += [solo_info['avgtempo'][i]]
 
     # Get data from each melid
     data = {}
     for i, m in enumerate(melid):
         data[i] = melody[melody['melid'] == m]
-
-    return data
+    return data, avgtempo
 
 def create_data_bin(bin_size, instrument = 'p', contour_len = 4, pdf = False):
     data_pitch = []
@@ -79,87 +83,159 @@ def create_data_bin(bin_size, instrument = 'p', contour_len = 4, pdf = False):
 
 def create_data(instrument = 'p', contour_len = 4, pdf = False):
     data_pitch = []
-    data_contour = []
     data_duration = []
+    data_step = []
+    data_end = []
 
     # Get all instrument (default piano) data 
-    df = find_instruments(instrument)
-
-    for data_set in df:
+    df, bpms = find_instruments(instrument)
+    for data_set in df: # iterate through each solo
         # pitch and duration
-        
-
         # create contout
-        k = df[data_set]['duration'].keys()[0]
-        for i in range(len(df[data_set]['pitch'])):
-            data_duration += [df[data_set]['duration'][i + k]]
-            data_pitch += [df[data_set]['pitch'][i + k]]
-            if i > (contour_len - 1):
-                data_contour += [np.average(
-                    df[data_set]['pitch'][(i - contour_len):i])]
+        # k = df[data_set]['duration'].keys()[0] # duration of first event
+        # for i in range(len(df[data_set]['pitch'])): 
+        #     data_duration += [df[data_set]['duration'][i + k]]
+        #     data_pitch += [df[data_set]['pitch'][i + k]]
+        #     if i > (contour_len - 1):
+        #         data_contour += [np.average(
+        #             df[data_set]['pitch'][(i - contour_len):i])]
+        #     else:
+        #         data_contour += [0]
+        step = 0
+        for i in range(len(df[data_set]['pitch'])): # count for each pitch in the df
+            if i == len(df[data_set]['pitch']) - 1: # at the end
+                data_end.append(1)
             else:
-                data_contour += [0]
-    print(data_duration[:5])
+                data_end.append(0)
+            data_duration.append(df[data_set]['duration'].iloc[i]) # add duration of current event
+            data_pitch.append(df[data_set]['pitch'].iloc[i]) # add pitch of current event
+            data_step.append(step)
+            if i < len(df[data_set]['pitch']) - 1:
+                step = df[data_set]['onset'].iloc[i + 1] - df[data_set]['onset'].iloc[i] - df[data_set]['duration'].iloc[i] # length of silence after current note
+    
     # Convert data to either pandas df or to torch tensor
-    df = np.array([data_pitch, data_contour, data_duration]).T
+    df = np.array([data_pitch, data_step, data_duration, data_end]).T
     if pdf:
-        df = pd.DataFrame(df, columns=['pitch','contour'])
+        df = pd.DataFrame(df, columns=['pitch','step','duration', 'end'])
     else:
         df = torch.from_numpy(df).float()
 
     return df
 
+def make_sequences(df, sequence_length=16):
+    print(df.shape)
+    num_sequences = len(df) - sequence_length # find the number of overlapping sequences
+    # X = np.empty((num_sequences, 4))
+    # y = np.empty((num_sequences, sequence_length, 4))
+    
+    X = []
+    y = []
+    for i in range(num_sequences):
+        X.append(df[i:i+sequence_length]) # add the sequence as the data
+        y.append(df[i+sequence_length]) # add the label
+    return X, y
+    
+
 class rnn_model(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size, num_layers=1):
         super(rnn_model, self).__init__()
-        self.rnn = nn.RNN(input_size, hidden_size, batch_first=True)
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
     
-    def forward(self, x):
-        h0 = torch.zeros(1, x.size(0), hidden_size).to(x.device)
-        out, _ = self.rnn(x, h0)
+    def forward(self, x, hidden):
+        print(f"x,shape: {x.shape}")
+        out, hidden = self.lstm(x, hidden)
+        print(f"out: {out}")
+        # print(out.shape)
+        # print(out)
+        # h0 = torch.zeros(1, x.size(0), hidden_size).to(x.device)
+        # out, _ = self.rnn(x, h0)
+        # out = self.fc(out[:, -1, :])
         out = self.fc(out)
-        return out
+        return out, hidden
+    
+    def init_hidden(self):
+        print(self.num_layers, self.hidden_size)
+        h0 = torch.zeros(self.num_layers, self.hidden_size)
+        c0 = torch.zeros(self.num_layers, self.hidden_size)
+        print(f'h0: {h0.shape}')
+        print(f'c0: {c0.shape}')
+        print(h0)
+        return (h0, c0)
 
-def train_model(X, y, model):
-    loss_func = nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=rate)
+def train_model(X, y, model, rate, epochs):
+    loss_func = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=rate)
 
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+    
     # Training loop
-    num_epochs = 100
-    for epoch in range(num_epochs):
-        model.train()
-        outputs = model(X.unsqueeze(2))  # Add a dimension for input size
-        loss = loss_func(outputs, y.unsqueeze(2))
-        
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        
+    for epoch in range(epochs):
+        for Xi, yi in zip(X, y):
+            print(Xi, yi)
+            print(Xi.shape)
+            # model.train()
+            # outputs = model(X.unsqueeze(2))  # Add a dimension for input size
+            # loss = loss_func(outputs, y.unsqueeze(2))
+            
+            # optimizer.zero_grad()
+            # loss.backward()
+            # optimizer.step()
+            Xi = Xi.to(device)
+            yi = yi.to(device)
+            hidden = model.init_hidden()
+            outputs, hidden = model(Xi, hidden)
+            print(f'output: {outputs.shape}')
+            print(f'yi: {yi.shape}')
+            print(outputs, yi)
+            loss = loss_func(outputs, yi)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
         if (epoch + 1) % 10 == 0:
-            print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+            print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
+
+def test_model(model, start_sequence, max_length):
+    model.eval()
+    input_seq = torch.tensor(start_sequence, dtype=torch.float32).unsqueeze(0)
+    generated_sequence = start_sequence
+    hidden = model.init_hidden(1)
+    
+    for _ in range(max_length):
+        output, hidden = model(input_seq, hidden)
+        _, predicted_event = torch.max(output.data, 1) # choose most probable next event
+        predicted_event = predicted_event.item() # get data from tensor
+        generated_sequence.append(predicted_event) # add predicted event to sequence
+        input_seq.cat(torch.tensor(predicted_event)) # add predicted event to next input
+    
+    return generated_sequence        
 
 if __name__ == "__main__":
     d = create_data()
-    #print(d[:20])
-    X = d[:-1]
-    y = d[1:]
     
-    input_size = 1
-    hidden_size = 200
-    output_size = 2
+    input_size = 16
+    hidden_size = 256
+    output_size = 16
     rate = 0.001
-    model = rnn_model(input_size, hidden_size, output_size)
-    print(X.shape)
+    epochs = 500
+    sequence_length = 16
+    max_length = 100 # number of new events to generate in composition
+    X, y = make_sequences(d, sequence_length)
+    model = rnn_model(input_size, hidden_size, output_size, 2)
 
-    train_model(X, y, model)
-
-    model.eval()
-    with torch.no_grad():
-        predictions = model(X.unsqueeze(2)).squeeze(2).numpy()
-
-    print(predictions)
-
-    #makeMidi(predictions)
+    train_model(X, y, model, rate, epochs)
+    
+    start_sequence = X[0] # take first sequence of training set as test
+    sequence = test_model(model, start_sequence, max_length)
+    print(sequence)
+    # print(predictions)
+    # makeMidi(predictions)
 
     
--- makemidi.py
+++ makemidi.py
@@ -1,25 +1,42 @@
 from midiutil.MidiFile import MIDIFile
 from midi2audio import FluidSynth
-
+from plotnine import *
+import pandas
 
 def makeMidi(d):
     # create your MIDI object
     mf = MIDIFile(1)     # only 1 track
     track = 0   # the only track
-
     time = 0    # start at the beginning
     mf.addTrackName(track, time, "Sample Track")
-    mf.addTempo(track, time, 120)
+    mf.addTempo(track, time, 60)
 
     # add some notes
     channel = 0
     volume = 100
-
+    # data = {}
+    # times = []
+    # pitches = []
+    file_index = 0
     for i in d:
         print(i)
-        mf.addNote(track, channel, int(i[0]), time, i[2], volume)
-        time += i[2]
-
-    # write it to disk
-    with open("output.mid", 'wb') as outf:
-        mf.writeFile(outf)
+        pitch = int(i[0])
+        step = i[1]
+        duration = i[2]
+        end = i[3]
+        
+        if end == 1: # end of solo
+            # print(f'adding note: {track, channel, pitch, time, duration, volume}')
+            mf.addNote(track, channel, pitch, time, duration, volume)
+            with open(f"output_{file_index}.mid", 'wb') as outf:
+                mf.writeFile(outf)
+            print(f"created output_{file_index}.mid")
+            file_index += 1
+            time = 0
+            mf = MIDIFile(1) # initialize new midi file
+            mf.addTrackName(track, time, "Sample Track")
+            mf.addTempo(track, time, 60)
+        else:
+            # print(f'adding note: {track, channel, pitch, time, duration, volume}')
+            mf.addNote(track, channel, pitch, time, duration, volume)
+            time += duration + step
